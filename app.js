e8dd3090b5402a3659f7b661763c9d6186251a2b
anderson <andersonrsilva22@gmail.com>
2022-02-18 11:09:08 -0300

    realizado correção

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let paises = [0];
+let paises = [];
+
+paises.push('Brasil', 'Argentina', 'Colombia','Peru','Equador','Bosnia','Venezuela','Bolívia','Uruguai','Guiana','Suriname','Paraguai','Guiana Francesa');
+
+console.table(paises);
+
+console.log("imprimindo as posições")
